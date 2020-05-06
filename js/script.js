d1d60a140e151bf0d09bbb05917fe6fd4047c10c
Firefly718 <System.BLA.16@yandex.ru>
2020-05-06 13:07:57 +0300

    added first slider

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// slick's script
+// $(document).ready(function(){
+//   $('.slider').slick({
+//     setting-name: setting-value
+//   });
+// });
+
+
+// another works script
+$(function(){
+  $('.slider').slick();
+});
