cbd908506bf238e6f954b50ca608c15c60383af0
Firefly718 <System.BLA.16@yandex.ru>
2020-05-06 15:43:13 +0300

    edits for slider

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@
 
 
 // another works script
-$(function(){
-  $('.slider').slick();
+// $(function(){
+//   $('.slider').slick({
+//     // autoplay = true;
+//     // dots = true;
+//   });
+// });
+
+$('.slider').slick({
+  autoplay: false,
+  dots: true
+  // nextArrow: 'src: ../img/slider/right-arrow.svg'
+  // prevArrow: '<div class="arrow arrow--left"></div>'
+  // infinite: true
 });
