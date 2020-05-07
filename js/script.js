a4975eae69a516a0decc2f4321acccc8bea66c8d
Firefly718 <System.BLA.16@yandex.ru>
2020-05-07 19:32:31 +0300

    finished slider in posts__item

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $('.slider').slick({
   autoplay: true,
   dots: true,
+  fade: true,
+  autoplaySpeed: 4000,
+  cssEase: 'linear',
   prevArrow: '<span class="prewArrow"><span>',
   nextArrow:'<span class="nextArrow"><span>'
 });
@@ -9,7 +12,14 @@ $('.posts__slider').slick({
   autoplay: false,
   dots: false,
   slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: '<span class="prewArrow"><span>',
-  nextArrow:'<span class="nextArrow"><span>'
+  slidesToScroll: 1
+  // prevArrow: '<span class="prewArrow"><span>',
+  // nextArrow:'<span class="nextArrow"><span>'
+});
+
+$('.posts__slider--item').slick({
+  autoplay: false,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
 });
